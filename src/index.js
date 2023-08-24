c010f2b02bb5abf0b8121e7f1a552994a02610d8
Matthew Baer <matt.baer77@gmail.com>
2023-08-23 23:11:47 -0400

    completed index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 
 import React from 'react';
+import foods from './foods'
+import {choice, remove} from './helpers'
+
+const randomFruit = choice(foods)
+const removedFruit = remove(foods, randomFruit)
+
+console.log(`I'd like one ${randomFruit} please`)
+console.log(`Here you go: ${randomFruit}`)
+console.log(`Delicious, May I have another?`)
+console.log(`I am sorry, we are all out. We have ${foods.length} left`)
